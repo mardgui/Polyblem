14b020dd812855ac14830cafe308e5ce746bc430
Florian Salord <florian.salord@etu.unice.fr>
2018-04-16 17:38:16 +0200

    Added Declaration activity

---
+++ app/src/main/java/fr/unice/polytech/polyblem/declaration/DeclarationActivity.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.polyblem.declaration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fr.unice.polytech.polyblem.R;
+
+/**
+ * Created by Florian on 16/04/2018
+ */
+
+public class DeclarationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_declaration);
+    }
+}
