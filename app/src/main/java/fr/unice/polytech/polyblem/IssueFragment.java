20290585c23ab483d13340dfbc114792969d6e26
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2018-05-11 14:42:26 +0200

    slider fonctionnel !!!

--- app/src/main/java/fr/unice/polytech/polyblem/IssueFragment.java
+++ app/src/main/java/fr/unice/polytech/polyblem/IssueFragment.java
@@ -3,9 +3,12 @@ package fr.unice.polytech.polyblem;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,10 +16,8 @@ import java.util.List;
 import fr.unice.polytech.polyblem.bdd.Database;
 import fr.unice.polytech.polyblem.model.Issue;
 import fr.unice.polytech.polyblem.model.Photo;
+import me.relex.circleindicator.CircleIndicator;
 
-/**
- * Created by user on 01/05/2018.
- */
 
 public class IssueFragment extends Fragment {
 
@@ -25,6 +26,8 @@ public class IssueFragment extends Fragment {
     public IssueFragment() {
     }
 
+    private ViewPager mPager;
+    private int currentPage = 0;
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         TextView title = getView().findViewById(R.id.title);
@@ -33,8 +36,16 @@ public class IssueFragment extends Fragment {
         TextView categorie = getView().findViewById(R.id.categorie);
         categorie.setText(issue.getCategory());
 
+        ImageView urgency = getView().findViewById(R.id.urgency);
+
+        urgency.setImageResource(issue.getUrgency().getId());
+
         Database database = new Database(getContext());
         List<Photo> photoList = database.getPictures(issue);
+
+        Log.i("IssueFra", photoList.size() + " nb photos");
+
+        initSlides(photoList);
 
         TextView description = getView().findViewById(R.id.description);
         description.setText(issue.getDescription());
@@ -55,6 +66,16 @@ public class IssueFragment extends Fragment {
 
     }
 
+    private void initSlides(final List<Photo> photos) {
+
+        mPager = getView().findViewById(R.id.pager);
+        mPager.setAdapter(new ImageAdapter(getContext(),photos));
+        CircleIndicator indicator = getView().findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         if (container != null) {
@@ -65,7 +86,6 @@ public class IssueFragment extends Fragment {
         if (bundle != null) {
             issue = bundle.getParcelable("Issue");
         }
-        View view = inflater.inflate(R.layout.issue, container, false);
-        return view;
+        return inflater.inflate(R.layout.issue, container, false);
     }
 }
